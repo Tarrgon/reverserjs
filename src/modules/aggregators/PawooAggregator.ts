1489a05008fea33a53f44b67f2a8f6fb30a82128
DontTalkToMeThx <61888458+DontTalkToMeThx@users.noreply.github.com>
2024-07-28 15:36:20 -0400

    Fix up names

--- src/modules/aggregators/PawooAggregator.ts
+++ src/modules/aggregators/PawooAggregator.ts
@@ -8,31 +8,35 @@ import { ObjectId, WithId, Document } from "mongodb"
 import AggregationManager, { AggregationJobData } from "../AggregationManager"
 import ArtistURL from "../ArtistURL"
 import ImageDownloader from "../ImageDownloader"
-import { parse } from "node-html-parser"
-import CohostScraper from "../customScrapers/CohostScraper"
+import MastodonScraper from "../customScrapers/MastodonScraper"
+import Job from "../Job"
 
-class CohostAggregator implements Aggregator {
-  index: number = 48
+class PawooAggregator implements Aggregator {
+  index: number = 29
   manager: AggregationManager
 
-  host: string = "cohost"
-  displayName: string = "Cohost"
-  homepage: string = "https://cohost.org/"
+  host: string = "pawoo"
+  displayName: string = "Pawoo"
+  homepage: string = "https://pawoo.net"
 
   galleryTemplates: RegExp[] = [
-    `cohost\\.org\\/${Globals.siteArtistIdentifier}`,
+    `pawoo\\.net\\/@${Globals.siteArtistIdentifier}`
   ].map(r => new RegExp(`${Globals.prefix}${r}\\/?${Globals.remaining}\\??${Globals.remaining}#?${Globals.remaining}`))
 
-  // usernameIdentifierRegex: RegExp = /[a-zA-Z0-9_\-]{3,63}/
-  submissionTemplate: string = "https://cohost.org/{siteArtistIdentifier}/post/{siteSubmissionIdentifier}"
+  usernameIdentifierRegex: RegExp = /[a-zA-Z0-9_]{1,30}/
+
+  submissionTemplate: string = "https://pawoo.net/statuses/{siteSubmissionIdentifier}"
 
   ready: boolean = false
   inUse: boolean = false
   canFetch: boolean = true
   canSearch: boolean = true
 
+  scraper: MastodonScraper
+
   constructor(manager: AggregationManager) {
     this.manager = manager
+    this.scraper = new MastodonScraper("https://pawoo.net/api/v1")
   }
 
   async createJobData(artistUrl: ArtistURL): Promise<AggregationJobData> {
@@ -45,25 +49,29 @@ class CohostAggregator implements Aggregator {
     let promises: Promise<any>[] = []
 
     try {
-      for await (let media of CohostScraper.getMedia(artistUrl)) {
+      for await (let media of this.scraper.getMedia(artistUrl)) {
         if (promises.length >= 100) {
           await Promise.all(promises)
           promises.length = 0
         }
 
-        // Special case, they aren't sorted, so just skip them instead of breaking
-        if (media.mediaUrls.length <= 0 || media.createdAt < latestDate) {
+        if (media.createdAt < latestDate) {
+          break
+        }
+
+        if (media.media.length <= 0) {
           continue
         }
 
-        for (let i = 0; i < media.mediaUrls.length; i++) {
-          let url = media.mediaUrls[i]
+        for (let i = 0; i < media.media.length; i++) {
+          let mastodonMedia = media.media[i]
+          let url = mastodonMedia.url
           let p = ImageDownloader.queueDownload(url)
 
           p.then(async (id) => {
             if (id) {
-              let imageData = { ...id, source: this.submissionTemplate.replace("{siteArtistIdentifier}", artistUrl.urlIdentifier).replace("{siteSubmissionIdentifier}", media.id), directLinkOffsite: url, offsiteId: `${media.id}_${i}` }
-              let prom = Submission.create(artistUrl._id, imageData.offsiteId, imageData.source, imageData.md5, media.title, media.description, media.createdAt, imageData.width, imageData.height, imageData.fileSize, imageData.directLinkOffsite, imageData.extension)
+              let imageData = { ...id, source: this.submissionTemplate.replace("{siteArtistIdentifier}", artistUrl.urlIdentifier).replace("{siteSubmissionIdentifier}", media.id), offsiteId: mastodonMedia.id }
+              let prom = Submission.create(artistUrl._id, imageData.offsiteId, imageData.source, imageData.md5, "", mastodonMedia.description.length == 0 ? media.description : `${media.description}\n\n${mastodonMedia.description}`, media.createdAt, imageData.width, imageData.height, imageData.fileSize, url, imageData.extension)
               promises.push(prom)
             } else {
               console.error("NO IMAGE DATA")
@@ -126,8 +134,8 @@ class CohostAggregator implements Aggregator {
   }
 
   async getApiIdentifier(urlIdentifier: string): Promise<string | null> {
-    return urlIdentifier
+    return await this.scraper.getApiIdentifier(urlIdentifier)
   }
 }
 
-export default CohostAggregator
+export default PawooAggregator
